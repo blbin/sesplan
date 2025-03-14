7feade0ddccc2e97b5e3bb84b4292f26b24d31e0
Patrik Trejtnar <imail@patriktrejtnar.cz>
2025-03-14 04:21:43 +0100

    TLS cert

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -5,3 +5,6 @@ from app.core.config import settings
 app = FastAPI(title="Sesplan API")
 app.include_router(api_router, prefix="/api")
 
+@app.get("/")
+async def root():
+    return {"message": "Hello from FastAPI!"}
