86249930acd63191f0295331ab1420ae68421d07
Patrik Trejtnar <imail@patriktrejtnar.cz>
2025-04-29 12:48:48 +0200

    CampaignsView.vue route fix v1.2

--- frontend/src/views/dashboard/CampaignsView.vue
+++ frontend/src/views/dashboard/CampaignsView.vue
@@ -18,7 +18,7 @@
         <li v-for="campaign in campaigns" :key="campaign.id" class="item-list-item">
           <div class="item-info">
             <h2>
-              <router-link :to="`/dashboard/campaigns/${campaign.id}`" class="item-link">
+              <router-link :to="`/d/campaigns/${campaign.id}`" class="item-link">
                 {{ campaign.name }}
               </router-link>
             </h2>
